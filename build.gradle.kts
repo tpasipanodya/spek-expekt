42e3dfde05a329d79dac27d97e18d5da13512709
pasitaf <tmpasipanodya@gmail.com>
2021-04-04 13:24:29 -0400

    c

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,30 @@
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jfrog.gradle.plugin.artifactory.dsl.PublisherConfig
+import org.jfrog.gradle.plugin.artifactory.dsl.ResolverConfig
+import groovy.lang.GroovyObject
 
 plugins {
 	kotlin("jvm") version "1.4.32"
-	id("com.github.johnrengelman.shadow") version "6.1.0"
+	id("com.jfrog.artifactory") version "4.21.0"
+	`maven-publish`
 	idea
 }
 
 group = "com.taff"
-version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_15
+version = "0.0.1" + (if (isSnapshotBuild()) "-SNAPSHOT" else "")
+java.sourceCompatibility = JavaVersion.VERSION_14
 
 repositories {
 	jcenter()
 	maven("https://jitpack.io")
+	maven {
+		name = "JFrog"
+		url = uri("https://pasitaf.jfrog.io/artifactory/releases")
+		credentials {
+			username = System.getenv("ARTIFACTORY_USER")
+			password = System.getenv("ARTIFACTORY_PASSWORD")
+		}
+	}
 }
 
 configurations {
@@ -39,12 +50,71 @@ dependencies {
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "15"
+		jvmTarget = "14"
 	}
 }
 
 tasks.withType<Test> { useJUnitPlatform() }
 
-tasks.withType<ShadowJar> {
-	archiveFileName.set("${rootProject.name}.${rootProject.version}.jar")
+publishing {
+	publications {
+		create<MavenPublication>("jar") {
+			this.groupId = project.group.toString()
+			this.artifactId = project.name
+			this.version = project.version.toString()
+
+			artifact("$buildDir/libs/${project.name}-${project.version}.jar")
+
+			pom {
+				name.set("project.name")
+				description.set("#{project.name} $version - Lightweight test utilities for hephaestus projects")
+				url.set("https://github.com/tpasipanodya/hephaestus-test")
+
+				licenses {
+					license {
+						name.set("The Apache Software License, Version 2.0")
+						url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+					}
+				}
+
+				developers {
+					developer {
+						name.set("Tafadzwa Pasipanodya")
+						email.set("tmpasipanodya@gmail.com")
+					}
+				}
+
+				scm {
+					connection.set("scm:git:git://github.com/tpasipanodya/hephaestus-test.git")
+					developerConnection.set("scm:git:ssh://github.com/tpasipanodya/hephaestus-test.git")
+					url.set("http://github.com/tpasipanodya/hephaestus-test/tree/main")
+				}
+			}
+
+		}
+	}
 }
+
+artifactory {
+	setContextUrl("https://pasitaf.jfrog.io/artifactory/")
+
+	publish(delegateClosureOf<PublisherConfig> {
+
+		repository(delegateClosureOf<GroovyObject> {
+			setProperty("repoKey", "snapshots")
+			setProperty("username", System.getenv("ARTIFACTORY_USER"))
+			setProperty("password", System.getenv("ARTIFACTORY_PASSWORD"))
+			setProperty("maven", true)
+		})
+
+		defaults(delegateClosureOf<GroovyObject> {
+			invokeMethod("publications", "jar")
+		})
+	})
+
+	resolve(delegateClosureOf<ResolverConfig> {
+		setProperty("repoKey", "hephaestus-test")
+	})
+}
+
+fun isSnapshotBuild() = System.getProperty("IS_SNAPSHOT_BUILD")?.toBoolean() == true
