5e06e253efea257216d93884fd7a03d08ff6c483
Tafadzwa Pasipanodya <tmpasipanodya@gmail.com>
2021-12-11 12:14:40 -0500

    c

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 }
 
 group = "io.taff"
-version = "0.3.0${ if (isReleaseBuild()) "" else "-SNAPSHOT" }"
+version = "1.0.0${ if (isReleaseBuild()) "" else "-SNAPSHOT" }"
 java.sourceCompatibility = JavaVersion.VERSION_16
 
 repositories {
