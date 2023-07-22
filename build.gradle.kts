b884302f4ab8a6f68bca78ff60afd1ec6fce39db
Tafadzwa Pasipanodya <tmpasipanodya@gmail.com>
2023-07-22 03:11:31 -0400

    c

--- build.gradle.kts
+++ build.gradle.kts
@@ -66,7 +66,7 @@ publishing {
 	repositories {
 		maven {
 			name = "GitHubPackages"
-			url = uri("https://maven.pkg.github.com/tmpasipanodya/spek-expekt")
+			url = uri("https://maven.pkg.github.com/tpasipanodya/spek-expekt")
 			credentials {
 				username = System.getenv("GITHUB_ACTOR")
 				password = System.getenv("GITHUB_TOKEN")
